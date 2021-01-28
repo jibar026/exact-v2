6458976066dae1e0c727b2d32bce43f72c06446f
jibar026 <59713182+jibar026@users.noreply.github.com>
2021-01-28 16:52:18 -0500

    updated ec2meta route

--- routes/ec2meta.js
+++ routes/ec2meta.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var router = express.Router();
-const axios = require('axios')
+// const axios = require('axios')
 
-const url = "http://169.254.169.254/latest/meta-data/"
+// const url = "http://169.254.169.254/latest/meta-data/"
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    try {
-        const response = axios.get(url)
-        const data = response.data
-        console.log(data)
-        res.send(json(data))
-      } catch (error) {
-        console.log(error)
-      }
+    // try {
+    //     const response = axios.get(url)
+    //     const data = response.data
+    //     console.log(data)
+    //     res.send(data)
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    res.json([{
+      id: 'ip',
+      value: req.ip
+    }, {
+      id: 'hostname',
+      value: req.hostname
+    }]);
 });
 
 module.exports = router;
